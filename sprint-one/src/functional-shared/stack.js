40ab9b5a32e634b2bc03c5c84f064eb6252b89fa
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 14:50:44 -0800

    Implemented .pop method

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -14,12 +14,15 @@ var makeStack = function() {
 var stackMethods = {};
 
 stackMethods.push = function(value) {
-  this.storage[count] = value;
+  this.storage[this.count] = value;
   this.count++;
 };
 
 stackMethods.pop = function() {
-
+  this.count--;
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
 };
 
 stackMethods.size = function() {
