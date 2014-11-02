e221707a99ff57700e76d157357292f8d50a9fa2
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 14:56:32 -0800

    Layout all methods of queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,35 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.count = 0;
+  newQueue.storage = {};
+
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+
+};
+
+queueMethods.isEmpty = function() {
+
+};
+
+
+
 
 
 
