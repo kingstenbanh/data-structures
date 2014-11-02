cf733c57a87e6abd4ef206359acc8e5e0e25de72
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 14:46:43 -0800

    Added method structures for stack constructor

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+
+  newStack.count = 0;
+  newStack.storage = {};
+
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+
+};
+
+stackMethods.isEmpty = function() {
+
+};
 
 
