9cedd1c37f03bfaaf76cb869e9a1b03e7c7c0433
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 15:19:01 -0800

    Implemented .dequeue method

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -13,18 +13,24 @@ var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
   this.storage[this.count] = value;
-  this.count++;
+  this.count++; 
 };
 
 queueMethods.dequeue = function() {
-
+  var result = this.storage[0];
+  delete this.storage[0];
+  for (var i = 0; i < this.count - 1; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  this.count--;
+  return result;
 };
 
 queueMethods.size = function() {
   return this.count < 0 ? 0 : this.count;
 };
 
-queueMethod.isEmpty = function() {
+queueMethods.isEmpty = function() {
   return this.count <= 0;
 };
 
