1c522ddb686b27dd0cbb13bb3f31fd112f38aa7f
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 15:07:41 -0800

    Layout the structure of methods for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+
+  newStack.count = 0;
+  newStack.storage = {};
+
+  return newStack;  
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+
+};
+
+stackMethods.isEmpty = function() {
+
+};
 
 
