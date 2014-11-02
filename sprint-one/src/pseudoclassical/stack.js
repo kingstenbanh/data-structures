734242aa8ae1148c691be17cfbcd49a018c6203b
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 15:26:46 -0800

    Implemented .isEmpty method

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,8 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
 Stack.prototype.push = function(value) {
@@ -16,5 +18,5 @@ Stack.prototype.size = function() {
 };
 
 Stack.prototype.isEmpty = function() {
-  
+  return this.count <= 0;
 };
