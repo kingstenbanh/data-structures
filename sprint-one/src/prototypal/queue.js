621d25b9d8043cf9a7104152d7ee14b9d0d1479f
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-02 15:09:25 -0800

    Layout the structure of methods in queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.count = 0;
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+
+};
+
+queueMethod.isEmpty = function() {
+
+};
 
 
