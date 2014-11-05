375f7d5630b95f7ee3f6fb9b6308e4f3aa23a89f
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-04 22:01:30 -0800

    designate a new tail when new nodes are added

--- sprint-two/src/functional/linkedList.js
+++ sprint-two/src/functional/linkedList.js
@@ -2,11 +2,18 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.ID = 0;
 
   list.addToTail = function(value){
+    var newTail = makeNode(value);
+    var oldTail = list.tail;
+    this.ID++;
+
+    list.tail = newTail;
   };
 
   list.removeHead = function(){
+
   };
 
   list.contains = function(target){
@@ -15,11 +22,12 @@ var makeLinkedList = function(){
   return list;
 };
 
-var makeNode = function(value){
+var makeNode = function(value, ID){
   var node = {};
 
   node.value = value;
   node.next = null;
+  node.ID = ID;
 
   return node;
 };
