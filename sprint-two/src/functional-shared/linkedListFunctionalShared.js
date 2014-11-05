c1ebb27e9cad5d78c06f04f7455fb42211168af2
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-04 22:46:45 -0800

    Changed to functional-shared instantiation style

--- sprint-two/src/functional-shared/linkedListFunctionalShared.js
+++ sprint-two/src/functional-shared/linkedListFunctionalShared.js
@@ -1,44 +1,11 @@
 var makeLinkedList = function(){
   var list = {};
+
   list.head = null;
   list.tail = null;
   list.ID = 0;
 
-  list.addToTail = function(value){
-    var newTail = makeNode(value, this.ID);
-    var oldTail = list.tail;
-
-    list.head = list.head || newTail;
-
-    if (oldTail) {
-      oldTail.next = newTail;
-    }
-
-    this.tail = newTail;
-    this[this.ID] = newTail;
-    this.ID++;
-  };
-
-  list.removeHead = function(){
-    if (list.head) {
-      var oldHead = list.head;
-      var newHead = list.head.next;
-
-      list.head = newHead;
-
-      delete list[oldHead.ID];
-      return oldHead.value;
-    }
-  };
-
-  list.contains = function(target){
-    for (var property in list) {
-      if (list[property].value === target) {
-        return true;
-      }
-    }
-    return false;
-  };
+  _.extend(list, listMethods);
 
   return list;
 };
@@ -52,6 +19,45 @@ var makeNode = function(value, ID){
 
   return node;
 };
+
+var listMethods = {};
+
+listMethods.addToTail = function(value){
+  var newTail = makeNode(value, this.ID);
+  var oldTail = list.tail;
+
+  list.head = list.head || newTail;
+
+  if (oldTail) {
+    oldTail.next = newTail;
+  }
+
+  this.tail = newTail;
+  this[this.ID] = newTail;
+  this.ID++;
+};
+
+listMethods.removeHead = function(){
+  if (list.head) {
+    var oldHead = list.head;
+    var newHead = list.head.next;
+
+    list.head = newHead;
+
+    delete list[oldHead.ID];
+    return oldHead.value;
+  }
+};
+
+listMethods.contains = function(target){
+  for (var property in list) {
+    if (list[property].value === target) {
+      return true;
+    }
+  }
+  return false;
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
