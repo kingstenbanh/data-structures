4a5ee20e10d095e72b30a5f9986b0dce96f72ec7
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-09 13:40:30 -0800

    Fig small bug

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,6 +14,34 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limits);
+  this._storage.set(i, null, k);
+};
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+var HashTable = function(){
+  this._limit = 8;
+  this._storage = makeLimitedArray(this._limit);
+};
+
+HashTable.prototype.insert = function(k, v){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+
+  // retrieve a bucket
+  this._storage.set(i, v, k);
+};
+
+HashTable.prototype.retrieve = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i, k);
+};
+
+HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, null, k);
 };
