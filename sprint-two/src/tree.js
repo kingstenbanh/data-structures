b666f262a64e35be14d88d0d52f5de0a823ecead
Kingsten Banh <kingstenbanh@gmail.com>
2014-11-09 09:37:31 -0800

    Implemented .contains method

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,7 +10,16 @@ Tree.prototype.addChild = function(value){
 };
 
 Tree.prototype.contains = function(target){
-
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
